8c79747eaa297df950045c4ab8b77a25af335c69
Neville4010 <niamhimurphy606@gmail.com>
2024-11-27 15:29:21 +0000

    changes values by selection of JComboBox Option.

--- BudgetBase.java
+++ BudgetBase.java
@@ -35,100 +35,134 @@ public class BudgetBase extends JPanel {    // based on Swing JPanel
     private JTextField rentField;      // Rent text field
     private JTextField otherSpendingField;     // Other spending field
     private JTextField totalIncomeField; // Total Income field
+    private String[] timePeriod = new String[] {"Weekly", "Monthly","Annually"};
 
     // constructor - create UI  (dont need to change this)
     public BudgetBase(JFrame frame) {
-        topLevelFrame = frame; // keep track of top-level frame
-        setLayout(new GridBagLayout());  // use GridBag layout
         initComponents();  // initalise components
     }
 
     // initialise componenents
     // Note that this method is quite long.  Can be shortened by putting Action Listener stuff in a separate method
     // will be generated automatically by IntelliJ, Eclipse, etc
-    private void initComponents() { 
+    private void initComponents() {
+
+        JComboBox<String> comboTimePeriod = new JComboBox<String>(timePeriod);
+        comboTimePeriod.setMaximumRowCount(4);
+        addComponent(comboTimePeriod, 0, 0);
+        add(comboTimePeriod);
+
+        comboTimePeriod.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent event){
+                JComboBox<String> comboTimePeriod = (JComboBox<String>) event.getSource();
+                String dateSelected = (String) comboTimePeriod.getSelectedItem();
+                if (dateSelected.equals("Weekly")) {
+                    System.out.println("Weekly view has been selected.");
+                    comboTimePeriod.setForeground(Color.GREEN);
+                    System.out.printf("Weekly amount: " + calculateTotalIncome());
+                    System.out.printf("Monthly amount: " + (calculateTotalIncome() * 4));
+                    System.out.printf("Yearly amount: " + (calculateTotalIncome() * 52));
+                }
+                else if (dateSelected.equals("Monthly")){
+                    System.out.println("Monthly view has been selected.");
+                    comboTimePeriod.setForeground(Color.GREEN);
+                    System.out.printf("Weekly amount: " + (calculateTotalIncome() / 4));
+                    System.out.printf("Monthly amount: " + calculateTotalIncome());
+                    System.out.printf("Yearly amount: " + (calculateTotalIncome() * 12));
+                }
+                else if (dateSelected.equals("Annually")){
+                    System.out.println("Annual view has been selected.");
+                    comboTimePeriod.setForeground(Color.GREEN);
+                    System.out.printf("Weekly amount: " + (calculateTotalIncome() / 52));
+                    System.out.printf("Monthly amount: " + (calculateTotalIncome() / 12));
+                    System.out.printf("Yearly amount: " + calculateTotalIncome());
+                }
+
+            }
+        });
 
         // Top row (0) - "INCOME" label
         JLabel incomeLabel = new JLabel("INCOME");
-        addComponent(incomeLabel, 0, 0);
+        addComponent(incomeLabel, 1, 0);
 
         // Row 1 - Wages label followed by wages textbox
         JLabel wagesLabel = new JLabel("Wages");
-        addComponent(wagesLabel, 1, 0);
+        addComponent(wagesLabel, 2, 0);
 
         // Row 2 - Other label followed by wages textbox
         JLabel otherLabel = new JLabel("Other");
-        addComponent(otherLabel, 2, 0);
+        addComponent(otherLabel, 3, 0);
 
 
         // set up text field for entering wages
         // Could create method to do below (since this is done several times)
         wagesField = new JTextField("", 10);   // blank initially, with 10 columns
         wagesField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
-        addComponent(wagesField, 1, 1);  
+        addComponent(wagesField, 2, 1);  
         
         // Row 2 - Other income label with input textbox following
         otherIncomeField = new JTextField("", 10);   // blank initially, with 10 columns
         otherIncomeField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
-        addComponent(otherIncomeField, 2, 1);
+        addComponent(otherIncomeField, 3, 1);
 
         // Row 3 - Loans label followed by loans textbox
         JLabel loansLabel = new JLabel("Loans");
-        addComponent(loansLabel, 3, 0);
+        addComponent(loansLabel, 4, 0);
 
         // set up text box for entering loans
         loansField = new JTextField("", 10);   // blank initially, with 10 columns
         loansField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
-        addComponent(loansField, 3, 1);
+        addComponent(loansField, 4, 1);
 
         // Row 4 - Expenditure label
         JLabel expenditureLabel = new JLabel("Expenditures");
-        addComponent(expenditureLabel, 4, 0);
+        addComponent(expenditureLabel, 6, 0);
 
         //Row 5 - Food outgoing label, followed by food outgoing field
         JLabel foodOutgoingLabel = new JLabel("Food");
-        addComponent(foodOutgoingLabel, 5, 0);
+        addComponent(foodOutgoingLabel, 7, 0);
 
         //Set up textbox to obtain food value
         foodField = new JTextField("", 10);
         foodField.setHorizontalAlignment(JTextField.RIGHT);
-        addComponent(foodField, 5, 1);
+        addComponent(foodField, 7, 1);
 
         //Row 6 - Rent outgoing label, followed by rent outgoing field
         JLabel rentOutgoingLabel = new JLabel("Rent");
-        addComponent(rentOutgoingLabel, 6, 0);
+        addComponent(rentOutgoingLabel, 8, 0);
 
         //Set up textbox to obtain rent value
         rentField = new JTextField("", 10);
         rentField.setHorizontalAlignment(JTextField.RIGHT);
-        addComponent(rentField, 6, 1);
+        addComponent(rentField, 8, 1);
 
         //Row 7 - Other outgoings label, followed by other outgoings field
         JLabel otherOutgoingsLabel = new JLabel("Other");
-        addComponent(otherOutgoingsLabel, 7, 0);
+        addComponent(otherOutgoingsLabel, 9, 0);
 
         //Set up textbox to obtain rent value
         otherSpendingField = new JTextField("", 10);
         otherSpendingField.setHorizontalAlignment(JTextField.RIGHT);
-        addComponent(otherSpendingField, 7, 1);
+        addComponent(otherSpendingField, 9, 1);
 
         // Row 8 - Total Income label followed by total income field
         JLabel totalIncomeLabel = new JLabel("Total Income");
-        addComponent(totalIncomeLabel, 8, 0);
+        addComponent(totalIncomeLabel, 10, 0);
 
         // set up text box for displaying total income.  Users cam view, but cannot directly edit it
         totalIncomeField = new JTextField("0", 10);   // 0 initially, with 10 columns
         totalIncomeField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
         totalIncomeField.setEditable(false);    // user cannot directly edit this field (ie, it is read-only)
-        addComponent(totalIncomeField, 8, 1);  
+        addComponent(totalIncomeField, 10, 1);  
 
         // Row 9 - Calculate Button
         calculateButton = new JButton("Calculate");
-        addComponent(calculateButton, 9, 0);  
+        addComponent(calculateButton, 11, 0);  
 
         // Row 10 - Exit Button
         exitButton = new JButton("Exit");
-        addComponent(exitButton, 10, 0);  
+        addComponent(exitButton, 11, 1);  
 
         // set up  listeners (in a spearate method)
         initListeners();
@@ -184,7 +218,7 @@ public class BudgetBase extends JPanel {    // based on Swing JPanel
         }
 
         // otherwise calculate total income and update text field
-        double totalIncome = (wages + otherIncome + loans) - (food + rent + otherSpending);
+        double totalIncome = ((wages + otherIncome + loans) - (food + rent + otherSpending));
         totalIncomeField.setText(String.format("%.2f",totalIncome));  // format with 2 digits after the .
         if (totalIncome < 0){
             totalIncomeField.setForeground(Color.RED);
@@ -231,15 +265,15 @@ public class BudgetBase extends JPanel {    // based on Swing JPanel
         //Display the window.
         frame.pack();
         frame.setVisible(true);
+
     }
- 
     // standard main class to set up Swing UI
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                createAndShowGUI(); 
+                createAndShowGUI();
             }
         });
     }
