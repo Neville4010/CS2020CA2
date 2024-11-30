06a947ea3faa5a3a9a1c0e62f1234da413af8b40
Neville4010 <niamhimurphy606@gmail.com>
2024-11-30 20:46:19 +0000

    display for income, outcome, and overall total for time period. Also, via use of Jcombo, amounts for weekly, monthly, and annually change dependant on user's selection via drop down.

--- BudgetBase.java
+++ BudgetBase.java
@@ -34,8 +34,15 @@ public class BudgetBase extends JPanel {    // based on Swing JPanel
     private JTextField foodField;      // Food text field
     private JTextField rentField;      // Rent text field
     private JTextField otherSpendingField;     // Other spending field
+    private JTextField totalSpendingField;     // Total spending field
+    private JTextField totalAmountField;       // Total amount field
     private JTextField totalIncomeField; // Total Income field
+    private JTextField weeklyTotalField; // Weekly total amount field
+    private JTextField monthlyTotalField; // Monthly total amount field
+    private JTextField yearlyTotalField; // Yearly total amount field
     private String[] timePeriod = new String[] {"Weekly", "Monthly","Annually"};
+    private double totalSpending;
+    private double totalIncome;
 
     // constructor - create UI  (dont need to change this)
     public BudgetBase(JFrame frame) {
@@ -60,23 +67,23 @@ public class BudgetBase extends JPanel {    // based on Swing JPanel
                 if (dateSelected.equals("Weekly")) {
                     System.out.println("Weekly view has been selected.");
                     comboTimePeriod.setForeground(Color.GREEN);
-                    System.out.printf("Weekly amount: " + calculateTotalIncome());
-                    System.out.printf("Monthly amount: " + (calculateTotalIncome() * 4));
-                    System.out.printf("Yearly amount: " + (calculateTotalIncome() * 52));
+                    weeklyTotalField.setText(String.format("%.2f",(calculateTotal())));
+                    monthlyTotalField.setText(String.format("%.2f",(calculateTotal() * 4.333333)));
+                    yearlyTotalField.setText(String.format("%.2f",(calculateTotal() * 52)));
                 }
                 else if (dateSelected.equals("Monthly")){
                     System.out.println("Monthly view has been selected.");
                     comboTimePeriod.setForeground(Color.GREEN);
-                    System.out.printf("Weekly amount: " + (calculateTotalIncome() / 4));
-                    System.out.printf("Monthly amount: " + calculateTotalIncome());
-                    System.out.printf("Yearly amount: " + (calculateTotalIncome() * 12));
+                    weeklyTotalField.setText(String.format("%.2f",(calculateTotal() / 4.333333)));
+                    monthlyTotalField.setText(String.format("%.2f",(calculateTotal())));
+                    yearlyTotalField.setText(String.format("%.2f",(calculateTotal() * 12)));
                 }
                 else if (dateSelected.equals("Annually")){
                     System.out.println("Annual view has been selected.");
                     comboTimePeriod.setForeground(Color.GREEN);
-                    System.out.printf("Weekly amount: " + (calculateTotalIncome() / 52));
-                    System.out.printf("Monthly amount: " + (calculateTotalIncome() / 12));
-                    System.out.printf("Yearly amount: " + calculateTotalIncome());
+                    weeklyTotalField.setText(String.format("%.2f",(calculateTotal() / 52)));
+                    monthlyTotalField.setText(String.format("%.2f",(calculateTotal() / 12)));
+                    yearlyTotalField.setText(String.format("%.2f",(calculateTotal())));
                 }
 
             }
@@ -150,19 +157,66 @@ public class BudgetBase extends JPanel {    // based on Swing JPanel
         JLabel totalIncomeLabel = new JLabel("Total Income");
         addComponent(totalIncomeLabel, 10, 0);
 
-        // set up text box for displaying total income.  Users cam view, but cannot directly edit it
+        // set up text box for displaying total income.  Users can view, but cannot directly edit it
         totalIncomeField = new JTextField("0", 10);   // 0 initially, with 10 columns
         totalIncomeField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
         totalIncomeField.setEditable(false);    // user cannot directly edit this field (ie, it is read-only)
         addComponent(totalIncomeField, 10, 1);  
 
+        // Row 8 - Total Spending label followed by total spending field
+        JLabel totalSpendingLabel = new JLabel("Total Spending");
+        addComponent(totalSpendingLabel, 11, 0);
+
+        // set up text box for displaying total spending.  Users can view, but cannot directly edit it
+        totalSpendingField = new JTextField("0", 10);   // 0 initially, with 10 columns
+        totalSpendingField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
+        totalSpendingField.setEditable(false);    // user cannot directly edit this field (ie, it is read-only)
+        addComponent(totalSpendingField, 11, 1); 
+
+        // Row 8 - Total Amount label followed by total income field
+        JLabel totalAmountLabel = new JLabel("Total Amount");
+        addComponent(totalAmountLabel, 12, 0);
+
+        // set up text box for displaying total amount.  Users can view, but cannot directly edit it
+        totalAmountField = new JTextField("0", 10);   // 0 initially, with 10 columns
+        totalAmountField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
+        totalAmountField.setEditable(false);    // user cannot directly edit this field (ie, it is read-only)
+        addComponent(totalAmountField, 12, 1);
+
+        // Row 8 - Total Amount label followed by total income field
+        JLabel weeklyTotalLabel = new JLabel("Weekly Total");
+        addComponent(weeklyTotalLabel, 13, 0);
+
+        weeklyTotalField = new JTextField("", 10);   // 0 initially, with 10 columns
+        weeklyTotalField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
+        weeklyTotalField.setEditable(false);    // user cannot directly edit this field (ie, it is read-only)
+        addComponent(weeklyTotalField, 14, 1);
+
+        // Row 8 - Total Amount label followed by total income field
+        JLabel monthlyTotalLabel = new JLabel("Monthly Total");
+        addComponent(monthlyTotalLabel, 14, 0);
+
+        monthlyTotalField = new JTextField("", 10);   // 0 initially, with 10 columns
+        monthlyTotalField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
+        monthlyTotalField.setEditable(false);    // user cannot directly edit this field (ie, it is read-only)
+        addComponent(monthlyTotalField, 14, 1); 
+
+        // Row 8 - Total Amount label followed by total income field
+        JLabel yearlyTotalLabel = new JLabel("Annual Total");
+        addComponent(yearlyTotalLabel, 15, 0);
+
+        yearlyTotalField = new JTextField("", 10);   // 0 initially, with 10 columns
+        yearlyTotalField.setHorizontalAlignment(JTextField.RIGHT) ;    // number is at right end of field
+        yearlyTotalField.setEditable(false);    // user cannot directly edit this field (ie, it is read-only)
+        addComponent(yearlyTotalField, 15, 1); 
+
         // Row 9 - Calculate Button
         calculateButton = new JButton("Calculate");
-        addComponent(calculateButton, 11, 0);  
+        addComponent(calculateButton, 13, 0);  
 
         // Row 10 - Exit Button
         exitButton = new JButton("Exit");
-        addComponent(exitButton, 11, 1);  
+        addComponent(exitButton, 14, 0);  
 
         // set up  listeners (in a spearate method)
         initListeners();
@@ -182,9 +236,13 @@ public class BudgetBase extends JPanel {    // based on Swing JPanel
         // calculateButton - call calculateTotalIncome() when pressed
         calculateButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                calculateTotalSpending();
                 calculateTotalIncome();
+                calculateTotal();
             }
         });
+
+        
 
     }
 
@@ -197,33 +255,65 @@ public class BudgetBase extends JPanel {    // based on Swing JPanel
 
     }
 
+
     // update totalIncomeField (eg, when Calculate is pressed)
     // use double to hold numbers, so user can type fractional amounts such as 134.50
     public double calculateTotalIncome() {
 
         // get values from income text fields.  value is NaN if an error occurs
+
         double wages = getTextFieldValue(wagesField);
         double otherIncome = getTextFieldValue(otherIncomeField);
         double loans = getTextFieldValue(loansField);
-        double food = getTextFieldValue(foodField);
-        double rent = getTextFieldValue(rentField);
-        double otherSpending = getTextFieldValue(otherSpendingField);
-
-
+    
         // clear total field and return if any value is NaN (error)
-        if (Double.isNaN(wages) || Double.isNaN(loans)) {
+        if (Double.isNaN(wages) || Double.isNaN(loans) || Double.isNaN(otherIncome)) {
             totalIncomeField.setText("");  // clear total income field
             wages = 0.0;
             return wages;   // exit method and do nothing
         }
 
         // otherwise calculate total income and update text field
-        double totalIncome = ((wages + otherIncome + loans) - (food + rent + otherSpending));
+        totalIncome = wages + otherIncome + loans;
         totalIncomeField.setText(String.format("%.2f",totalIncome));  // format with 2 digits after the .
         if (totalIncome < 0){
             totalIncomeField.setForeground(Color.RED);
         }
         return totalIncome;
+    }
+
+    public double calculateTotalSpending() {
+        double rent = getTextFieldValue(rentField);
+        double food = getTextFieldValue(foodField);
+        double otherSpending = getTextFieldValue(otherSpendingField);
+
+        if (Double.isNaN(rent) || Double.isNaN(food) || Double.isNaN(otherSpending)){
+          totalSpendingField.setText("");
+          rent = 0.0;
+          return rent;
+        }
+        totalSpending = rent + food + otherSpending;
+        totalSpendingField.setText(String.format("%.2f",totalSpending));  // format with 2 digits after the .
+
+        return totalSpending;
+    }
+
+    public double calculateTotal() {
+        // clear total field and return if any value is NaN (error)
+        if (Double.isNaN(totalSpending) || Double.isNaN(totalIncome)) {
+            totalAmountField.setText("");  // clear overall total field
+            totalSpending = 0.0;
+            return totalSpending;   // exit method and do nothing
+        }
+
+        // otherwise calculate overall total and update text field
+        double overallTotal = totalIncome - totalSpending;
+        totalAmountField.setText(String.format("%.2f",overallTotal));  // format with 2 digits after the .
+        if (overallTotal < 0){
+            totalAmountField.setForeground(Color.RED);
+        }
+
+        return overallTotal;
     }
 
     // return the value if a text field as a double
